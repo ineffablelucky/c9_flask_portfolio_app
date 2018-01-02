20b55af5e9824fe81ea237cb80699b73e1c414da
Lakshay Kumar <33530741+ineffablelucky@users.noreply.github.com>
2018-01-02 13:24:59 +0530

    str(finalword)

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def pig_latin_post():
 				     print(word + choose)
 				     finalword = word + choose
 				     print(finalword)
-				     return render_template('piglatin.html', result=finalword)
+				     return render_template('piglatin.html', result=str(finalword))
 			      else:
 				     for m in wordlistvowels:
 					    if m in vowels:
@@ -102,7 +102,7 @@ def pig_latin_post():
 				     finalword = new + "ay"
 				     print(finalword)
 
-				     return render_template('piglatin.html', result=finalword)
+				     return render_template('piglatin.html', result=str(finalword))
 
 @app.route('/time', methods=['GET','POST'])
 def time_post():
